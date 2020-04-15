b2b9974891bf02e5cc64a79143864f6b32240ae2
AdTekDev <nguyenngtu777@gmail.com>
2020-04-15 11:06:13 +0700

    Add files via upload

--- pyflask_001.py
+++ pyflask_001.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import  (
+    Flask,
+    render_template
+)
 
 app = Flask(__name__)
 
 @app.route('/')
 def  index():
     return "<h1> Flask 001 - Hi </h1>"
+
+@app.route('/login', methods=['GET', 'POST'])
+def  login():
+    return render_template("login.html")
+
+@app.route('/profile')
+def  profile():
+    return render_template("profile.html")
