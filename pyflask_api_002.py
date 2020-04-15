bfb1d9953e1e00ac19c01f205e4749219154ea17
AdTekDev <nguyenngtu777@gmail.com>
2020-04-15 11:30:39 +0700

    Add files via upload

---
+++ pyflask_api_002.py
@@ -0,0 +1,42 @@
+from flask import  (
+    Flask,
+    render_template,
+    request
+)
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask import jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def  index():
+    return "<h1> Flask 001 - Hi </h1>"
+
+@app.route('/login', methods=['GET', 'POST'])
+def  login():
+    return render_template("login.html")
+
+@app.route('/profile')
+def  profile():
+    return render_template("profile.html")
+
+class Employees(Resource):
+    def get(self):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select * from NhanVien") # This line performs query and returns json result
+        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+
+class Employees_Name(Resource):
+    def get(self, employee_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from NhanVien where MaNV =%d "  %int(employee_id))
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+api = Api(app)
+api.add_resource(Employees, '/employees') # Route_1
+api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_2
+
